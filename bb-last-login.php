dc5870815280759563214121408457474228d2dd
danzajac <128778504+danzajac@users.noreply.github.com>
2020-03-14 00:46:41 -0700

    init commit

---
+++ bb-last-login.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              andrewrhyand.com
+ * @since             1.0.0
+ * @package           Boxybird_Last_Login
+ *
+ * @wordpress-plugin
+ * Plugin Name:       BoxyBird Last Login
+ * Plugin URI:        #
+ * Description:       Adds timestamp for the last time a User logged-in.
+ * Version:           1.0.0
+ * Author:            Andrew Rhyand
+ * Author URI:        andrewrhyand.com
+ * License:           MIT License
+ * License URI:       https://opensource.org/licenses/MIT
+ * Text Domain:       boxybird-last-login
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define('BOXYBIRD_LAST_LOGIN', '1.0.0');
+
+/**
+ * The core plugin class
+ */
+
+require_once plugin_dir_path(__FILE__) . 'includes/BB_Last_Login.php';
+
+BB_Last_Login::init();
---
+++ includes/BB_Last_Login.php
@@ -0,0 +1,42 @@
+<?php
+
+class BB_Last_Login
+{
+    public static function init()
+    {
+        add_action('wp_login', [BB_Last_Login::class, 'updateTimestamp'], 10, 2);
+        add_filter('manage_users_columns', [BB_Last_Login::class, 'addAdminColumns']);
+        add_filter('manage_users_custom_column', [BB_Last_Login::class, 'addTimestamp'], 10, 3);
+        add_filter('manage_users_sortable_columns', [BB_Last_Login::class, 'makeSortable']);
+    }
+
+    public static function updateTimestamp($user_login, $user)
+    {
+        update_user_meta($user->ID, 'bb_last_login', date('Y-m-d H:i:s'));
+    }
+
+    public static function addAdminColumns($column)
+    {
+        $column['last_login'] = 'Last Login';
+
+        return $column;
+    }
+
+    public static function addTimestamp($value, $column_name, $user_id)
+    {
+        if ($column_name === 'last_login') {
+            $date = get_user_meta($user_id, 'bb_last_login', true);
+
+            return !empty($date) ? $date : 'N/A';
+        }
+
+        return $value;
+    }
+
+    public static function makeSortable($columns)
+    {
+        $columns['last_login'] = 'last_login';
+
+        return $columns;
+    }
+}
